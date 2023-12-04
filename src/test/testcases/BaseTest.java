f1ca36af2a589fec5ed9a0e6b93f047369461a43
Alexi118 <cuongnd118@gmail.com>
2023-12-04 11:28:28 +0700

    2nd commit

--- src/test/testcases/BaseTest.java
+++ src/test/testcases/BaseTest.java
@@ -1,5 +1,26 @@
 package testcases;
 
-public class BaseTest {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import pages.FirstPage;
 
+import utils.Constants;
+
+public class BaseTest {
+    protected WebDriver driver;
+    protected FirstPage firstPage;
+    @Parameters({"browserName"})
+    @BeforeMethod
+    public void setUp(String browserName) throws InterruptedException{
+        WebDriverManager.chromedriver().setup();
+        driver.manage().window().maximize();
+        driver.get(Constants.baseURL);
+    }
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
 }
