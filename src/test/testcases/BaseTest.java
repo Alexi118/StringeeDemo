2c9c3e3fe4b59840602c06a2dccf51126add8271
Alexi118 <cuongnd118@gmail.com>
2023-12-04 14:12:00 +0700

    complete base POM

--- src/test/testcases/BaseTest.java
+++ src/test/testcases/BaseTest.java
@@ -3,14 +3,18 @@ package testcases;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pages.FirstPage;
 
 import utils.Constants;
 
+import java.time.Duration;
+
 public class BaseTest {
     protected WebDriver driver;
+    protected WebDriverWait waitFor;
     protected FirstPage firstPage;
 
     @BeforeMethod
@@ -20,6 +24,7 @@ public class BaseTest {
         driver.manage().window().maximize();
         driver.get(Constants.baseURL);
 
+        waitFor = new WebDriverWait(driver, Duration.ofSeconds(10));
         firstPage = new FirstPage(driver);
     }
     @AfterMethod
--- src/test/testcases/FirstTest.java
+++ src/test/testcases/FirstTest.java
@@ -1,10 +1,11 @@
 package testcases;
 
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
 public class FirstTest extends BaseTest{
     @Test
     public void FirstTestCase(){
-
+        //waitFor.until(ExpectedConditions.visibilityOf(firstPage.firstElement));
     }
 }
