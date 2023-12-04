b4bb997c501a5b9b6a60c54e56278b53ac6c4856
Alexi118 <cuongnd118@gmail.com>
2023-12-04 13:14:24 +0700

    set up POM

--- src/test/testcases/BaseTest.java
+++ src/test/testcases/BaseTest.java
@@ -2,9 +2,9 @@ package testcases;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
 import pages.FirstPage;
 
 import utils.Constants;
@@ -12,12 +12,15 @@ import utils.Constants;
 public class BaseTest {
     protected WebDriver driver;
     protected FirstPage firstPage;
-    @Parameters({"browserName"})
+
     @BeforeMethod
-    public void setUp(String browserName) throws InterruptedException{
+    public void setUp() throws InterruptedException{
         WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(Constants.baseURL);
+
+        firstPage = new FirstPage(driver);
     }
     @AfterMethod
     public void tearDown(){
---
+++ src/test/testcases/FirstTest.java
@@ -0,0 +1,10 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class FirstTest extends BaseTest{
+    @Test
+    public void FirstTestCase(){
+
+    }
+}
