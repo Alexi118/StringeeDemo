37df984a877e5c30589f835e969ab5926a11342c
Alexi118 <cuongnd118@gmail.com>
2023-12-04 18:15:39 +0700

    fixing test02

--- src/test/pages/ContactPage.java
+++ src/test/pages/ContactPage.java
@@ -11,12 +11,13 @@ public class ContactPage extends BasePage{
 
     @FindBy(css = "a[href=\"/contact/add\"]")
     public WebElement add_contact_button;
-
     @FindBy(css = "#contactsContent tr:nth-child(1) td:nth-child(2) b")
     public WebElement firstContact_InList;
-
     @FindBy(css = "#contactsContent table")
     public WebElement table;
+    @FindBy(css="input[placeholder=\"Search by name, phone, email...\"]")
+    public WebElement input;
+
     public void action_clickOn_AddContacts(){
         add_contact_button.click();
     }
--- src/test/testcases/FirstTest.java
+++ src/test/testcases/FirstTest.java
@@ -5,21 +5,21 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class FirstTest extends BaseTest{
-    @Test
-    public void FirstTestCase(){
-        waitFor.until(ExpectedConditions.visibilityOf(loginPage.email_txt));
-        loginPage.action_logInWithCorrectUser();
-        waitFor.until(ExpectedConditions.urlMatches("https://automation-str.stringeex.com/dashboard/callin"));
-    }
+//    @Test
+//    public void FirstTestCase(){
+//        waitFor.until(ExpectedConditions.visibilityOf(loginPage.email_txt));
+//        loginPage.action_logInWithCorrectUser();
+//        waitFor.until(ExpectedConditions.urlMatches("https://automation-str.stringeex.com/dashboard/callin"));
+//    }
 
     @Test
-    public void SecondTestCase(){
+    public void SecondTestCase() throws InterruptedException {
         loginPage.action_logInWithCorrectUser();
         waitFor.until(ExpectedConditions.urlMatches("https://automation-str.stringeex.com/dashboard/callin"));
         homePage.action_clickOn_Contacts();
         contactPage.action_clickOn_AddContacts();
         contactAddPage.action_AddAContacts();
-        waitFor.until(ExpectedConditions.visibilityOf(contactPage.table));
+        waitFor.until(ExpectedConditions.visibilityOf(contactPage.firstContact_InList));
         Assert.assertEquals(contactPage.firstContact_InList.getText(),"Cuong");
     }
 }
